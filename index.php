8d54f9c3ce435cad62b6a8b3284aae7b63f33523
loic lepreux <lepreux.loic@bbox.fr>
2026-04-25 14:43:06 +0200

    Correction du fichier index

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
 
+require_once __DIR__ . '/vendor/autoload.php';
+
 use monApp\core\app;
-use monApp\core\tools;
 
-ini_set('display_errors', 1);
-require "vendor/autoload.php";
-
-tools::gets();
-app::db();
 app::page();
+
 echo app::getHtml();
